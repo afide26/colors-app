7667d4dc84d2e23a6d0dc6afa6c9feb3c55e5af5
Alan Fidelino <afidedev26@gmail.com>
2019-05-11 22:00:37 +1000

    Add ColorBox

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Palette from "./Palette";
+import Palette from "./components/Palette";
 import seedColors from "./seedColors";
 class App extends Component {
   render() {
     return (
       <div>
-        <Palette {...seedColors[4]} />
+        <Palette {...seedColors[2]} />
       </div>
     );
   }
