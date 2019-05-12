449711c3854e988af2e22f7a2d3441886b25dcfa
Alan Fidelino <afidedev26@gmail.com>
2019-05-12 18:58:07 +1000

    Add react-copy-to-clipboard to copy hex values

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "../styles/ColorBox.css";
 class ColorBox extends Component {
   render() {
     const { background, name } = this.props;
     return (
-      <div style={{ backgroundColor: background }} className="ColorBox">
-        <div className="copy">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background}>
+        <div style={{ backgroundColor: background }} className="ColorBox">
+          <div className="copy">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
+          <span className="see-more">More</span>
         </div>
-        <span className="see-more">More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
