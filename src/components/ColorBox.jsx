f94b519912ea601e13f79b8abd340ee7e305dd1e
Alan Fidelino <afidedev26@gmail.com>
2019-05-12 19:31:27 +1000

    Toggle copied state and add styles based on state change

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -2,11 +2,36 @@ import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import "../styles/ColorBox.css";
 class ColorBox extends Component {
+  state = {
+    copied: false
+  };
+
+  changeCopyState = () => {
+    this.setState(
+      {
+        copied: true
+      },
+      () => {
+        setTimeout(() => {
+          this.setState({ copied: false });
+        }, 1500);
+      }
+    );
+  };
   render() {
     const { background, name } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
-        <div style={{ backgroundColor: background }} className="ColorBox">
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{ background }} className="ColorBox">
+          <div
+            style={{ background }}
+            className={`copy-overlay ${copied && "show"}`}
+          />
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1>Copied!</h1>
+            <p>{background}</p>
+          </div>
           <div className="copy">
             <div className="box-content">
               <span>{name}</span>
